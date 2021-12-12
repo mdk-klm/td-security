77076674093e35c8e8b92b8d36d909da59e9c2ad
mdk-klm <49032958+mdk-klm@users.noreply.github.com>
2021-12-12 20:19:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {exec} = require('child_process');
 const app = express();
 
 
@@ -10,26 +10,46 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/users', (req, res, next) => {
+  
+  app.get('/trace', (req, res) => {
+    const file = req.query.file;
+  
+    const command = `git log --oneline ${file}`;
+  
+    exec(command, (err, output) => {
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+  
+      
+      res.send(output);
+    });
+  });
+
+app.get('/users', (req, res, next) => {
+    
     const user = [
       {
         id: '1',
         gender: 'male',
         lastName: 'georges',
         firstName :" moustaki",
-        price: 4900,
+        passwd: "youpi",
         userId: 'qsomihvqios',
       },
       {
-        id: '2',
+        "id": '2',
         gender: 'female',
         lastName: 'sand',
         firstName: 'georges',
-        price: 2900,
+        passwd: "youpi",
         userId: 'qsomihvqios',
       },
     ];
     res.status(200).json(user);
   });
+
+
 
 module.exports = app;
